79211e49d5f6456784b506d7b05be90a56e70057
Rodrigo Mtz <88547726+theRobert174@users.noreply.github.com>
2022-05-30 13:42:36 -0500

    Calendario desactualizado y versiones de otros

---
+++ src/app/pages/date-time/date-time.page.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-date-time',
+  templateUrl: './date-time.page.html',
+  styleUrls: ['./date-time.page.scss'],
+})
+export class DateTimePage implements OnInit {
+
+  fechaNaci: Date = new Date();
+  customYearValues = [2025, 2020, 2016, 2008, 2004, 2000, 1996];
+  customPickerOptions = {
+    buttons: [
+      {
+        text: 'Hola',
+        handler: ( event ) => {
+          console.log(event);
+        }
+      },
+      {
+        text: 'Mundo',
+        handler: () => {
+          console.log('!log');
+        }
+      }
+    ]
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  cambioFecha(event : any){
+    console.log(event);
+    console.log(new Date(event.detail.value));
+  }
+
+  confirm(){
+
+  }
+
+  reset(){
+    
+  }
+
+}
